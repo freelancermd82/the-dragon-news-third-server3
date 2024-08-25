66a4acf98e5fc9a4bf27422629e9ea5eaeabdc82
Your Name <freelancermd82@gmail.com>
2024-08-25 20:05:12 +0600

    categories id

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json')
 
 app.use(cors());
 
@@ -15,9 +16,35 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/news', (req, res) => {
+    res.send(news);
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNews);
+    }
+
+
 })
 
 
-app.listen(port, ()=> {
+
+
+app.listen(port, () => {
     console.log(`Dragon API is running on port: ${port}`)
 })
